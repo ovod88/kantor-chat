8380d57f24cb9059225111ac0147843c4351db01
Volodymyr Khrystenko <ovod88@bigmir.net>
2017-05-12 18:56:19 +0200

    Added EJS locals template

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
 var express = require('express');
 var app = express();
-var logger = require('logger/log')(module);
+var logger = require('./logger/log')(module);
 
-
-// var path = require('path');
-// var favicon = require('serve-favicon');
-// var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
-// var bodyParser = require('body-parser');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 // var index = require('./routes/index');
 // var users = require('./routes/users');
 
 // // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs-locals'));
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'ejs');
 
 // // uncomment after placing your favicon in /public
-// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
+if(app.get('env') == 'development') {//log request AT THE END OF REQUEST
+  app.use(logger('dev'));//different log formats
+} else {
+  app.use(logger('default'));
+}
+
+app.use(bodyParser.json());//parse application/json body sent from form to req.body object
+app.use(bodyParser.urlencoded({ extended: false }));//parse header parameters (for example GET) to req.query object
+app.use(cookieParser());//parse cookies into req.cookies object if there are any
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(req, resp) {
+  resp.render('index');
+});
 
 //Default route if express does not find route 
-
 app.use(function(req, resp, next) {
   if(req.url == '/forbidden') {
     next(new Error('Forbidden url'));//If next is called with argument - express automatically detects an error 
@@ -40,7 +49,7 @@ app.use(function(req, resp) {//Default route handler
   resp.status(404).send('No page for the provided url');
 });
 
-app.use(function(err, req, resp, next) {//Error handler
+app.use(function(err, req, resp, next) {//Error handler is called then error is thrown or next() is called this parameter
 
   if(app.get('env') == 'development') {//This parameter can be changed via NODE_ENV. By default development
     resp.status(500).send(err.stack);//Default express error handler
